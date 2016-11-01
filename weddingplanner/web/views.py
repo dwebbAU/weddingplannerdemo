df781d7c097d38597f6f9db0058aae4b23ad12e5
Ubuntu <ubuntu@ip-172-31-31-111.ap-southeast-2.compute.internal>
2016-11-01 01:33:14 +0000

    Push to dev

--- weddingplanner/web/views.py
+++ weddingplanner/web/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
 from core.models import Customer
 
+@login_required
 def index(request):
 	customers = Customer.objects.all()
 	return render(request,'index.html',{'customers':customers})
